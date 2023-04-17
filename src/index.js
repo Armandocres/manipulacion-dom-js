5259b676cd4cc035afa733bac572f5954dc592d7
Armandocres <armandocres99@gmail.com>
2023-04-17 14:10:31 -0600

    estilos y fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 const URL = 'https://platzi-avo.vercel.app/api/avo';
+const appNode = document.querySelector('#app');
+
+const formatPrice = (price) => {
+
+  const newPrice = new window.Intl.NumberFormat('en-EN', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(price)
+
+  return newPrice;
+}
 
 //web api
 window.fetch(URL)
@@ -8,15 +19,22 @@ window.fetch(URL)
     responseJson.data.forEach(item => {
       //crear imagen, titulo y precio
       const img = document.createElement('img');
+      img.src = `https://platzi-avo.vercel.app/${item.image}`;
+      img.className = 'h-22 w-22 md:h-24 md:w-24 rounded-full mx-auto ms:mx-0 ms:mr-6'
       const title = document.createElement('h2');
+      title.textContent = item.name;
+      title.className = 'text-lg';
       const price = document.createElement('div');
+      price.textContent = formatPrice(item.price);
+      price.className = 'text-gray-600';
       
       const container = document.createElement('div');
       //agregar al DOM
+      container.className = 'md:flex bg-white rounded-lg p-6 hover:bg-gray-300'
       container.append(img, title, price);
 
       ollNods.push(container);
     });
 
-    document.body.append(...ollNods);
+    appNode.append(...ollNods);
   }) // renderiza info browser
