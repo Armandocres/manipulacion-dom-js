882991f2ea164f967d380829dfa7a617cd2c4115
Armandocres <armandocres99@gmail.com>
2023-04-17 13:31:14 -0600

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const URL = 'https://platzi-avo.vercel.app/api/avo';
 
-console.log('Happy hacking :)')
+//web api
+window.fetch(URL)
+  .then(response => response.json()) //procesa la informacion y la convierte en json
+  .then(responseJson => {
+    const ollNods = [];
+    responseJson.data.forEach(item => {
+      //crear imagen, titulo y precio
+      const img = document.createElement('img');
+      const title = document.createElement('h2');
+      const price = document.createElement('div');
+      
+      const container = document.createElement('div');
+      //agregar al DOM
+      container.append(img, title, price);
+
+      ollNods.push(container);
+    });
+
+    document.body.append(...ollNods);
+  }) // renderiza info browser
